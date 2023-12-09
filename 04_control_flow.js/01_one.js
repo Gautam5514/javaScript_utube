6453fe0445dc5762c76bed596d033ba2707b196e
Gautam pandit <gautampandit797@gmail.com>
2023-12-10 01:28:22 +0530

    this si sthe new

---
+++ 04_control_flow.js/01_one.js
@@ -0,0 +1,24 @@
+//IF
+
+// const isUserLoggedIn = true
+// const tempreture = 43
+// if the constio is true then print the first console otherwise print the second console
+// if(tempreture == 45){
+//     console.log("Less then 50");
+// }
+// else{
+//     console.log("tempreture is greater then 43");
+// }
+// // This is no need any of the intruction this is always prints
+// console.log("You are the king");
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// scope use and using var => var is not use bcoz it is global variable
+const score = 200
+
+if(score > 100){
+    let power = "fly"
+    console.log(`user power ${power}`);
+}
+console.log(`user power ${power}`);
