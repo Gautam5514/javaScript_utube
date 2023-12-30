8fae6b3426bbf6515f6e816a962f9e35d67dad37
Gautam pandit <gautampandit797@gmail.com>
2023-12-30 11:19:59 +0530

    This is the second project in the javaScript

---
+++ 07_02_project/BMI_calculator.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const result = document.querySelector('#result');
+
+    if(height === '' || height < 0 || isNaN(height)) {
+        result.innerHTML = `sahi se bhar BC ${height}`;
+    }
+    else if(weight === '' || weight < 0 || isNaN(weight)) {
+        result.innerHTML = `Abhi na Bc sahi se Bhar ${weight}`;
+    }
+    else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
+        let statement = '';
+
+        if(bmi < 18.6) {
+            statement = 'Under Weight';
+        }
+        else if(bmi >= 18.6 && bmi <= 24.9) {
+            statement = 'Normal Range';
+        }
+        else{   
+            statement = 'Over Weight';
+        }
+
+        result.innerHTML = `<span>${bmi} kg/m2 </span><br>${statement}`;
+    }
+});
